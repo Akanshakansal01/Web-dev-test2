cc3085efcb588e9dcbaa639f45a56ee97993ddf6
Akanshakansal01 <akanshakansal025@gmail.com>
2022-11-30 11:59:55 +0530

    api

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 themeColour = document.querySelectorAll('[name="theme"]');
-      themeColour.forEach((theme) => {
-        theme.addEventListener("click",()=> localStorage.setItem("theme",theme.id));
-      });
+themeColour.forEach((theme) => {
+  theme.addEventListener("click", () =>
+    localStorage.setItem("theme", theme.id)
+  );
+});
 
-  function store() {
+function store() {
   store = localStorage.getItem("theme");
   themeColour.forEach((theme) => {
     if (theme.id === store) {
@@ -12,32 +14,32 @@ themeColour = document.querySelectorAll('[name="theme"]');
   });
 }
 document.onload = store();
-document.getElementById('start').addEventListener("click",display);
+document.getElementById("start").addEventListener("click", display);
 
-function display(){
-    var visit=document.getElementById('search').value;
-    req(visit);
+function display() {
+  var visit = document.getElementById("search").value;
+  req(visit);
 }
 var data;
-function req(api){
-    const url="https://api.tvmaze.com/search/shows?q=";
-    const api=new XMLHttpRequest;
-    api.onload=function(){
-        data=this.response;
-        // data.forEach((d)=>{
-        //     console.log(data);
-        // })
-        console.log(data);
-        var ch=document.getElementById('m');
-        var div=document.createElement('div');
-        ch.endChild(div);
-        div.className='movies';
-        
-    }
-    api.onerror=function(){
-        console.log('error');
-    }
-    api.open("GET",url+api);
-    api.send();
-    
-}      
+function req(a) {
+  const url = "https://api.tvmaze.com/search/shows?q=";
+  const p = new XMLHttpRequest();
+  p.onload = function () {
+    var c = document.getElementById("aa");
+    data = JSON.parse(this.response);
+    data.forEach((d) => {
+      console.log(d.show.image.original);
+      var im = d.show.image.original;
+      var div = document.createElement("div");
+      // c.appendChild(div);
+      div.className = "mov";
+      div.innerHTML = `<img src="${im}" alt="">`;
+      c.appendChild(div);
+    });
+  };
+  p.onerror = function () {
+    console.log("error");
+  };
+  p.open("GET", url + a);
+  p.send();
+}
